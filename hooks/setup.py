619db2b0bcdc7f2beb261507e0c1b74e2c8621ea
Whit Morriss <whit.morriss@canonical.com>
2015-01-05 11:23:44 -0600

    charmhelper bootstrap via pip

---
+++ hooks/setup.py
@@ -0,0 +1,20 @@
+import subprocess
+
+
+def pre_install():
+    """
+    Do any setup required before the install hook.
+    """
+    install_charmhelpers()
+    #subprocess.check_call(['pip', 'install', 'path.py'])
+
+
+def install_charmhelpers():
+    """
+    Install the charmhelpers library, if not present.
+    """
+    try:
+        import charmhelpers  # noqa
+    except ImportError:
+        subprocess.check_call(['apt-get', 'install', '-y', 'python-pip'])
+        subprocess.check_call(['pip', 'install', 'charmhelpers'])
