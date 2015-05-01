145de4127f9f87cc135d7f8d9e8ab7e1513bab23
Whit Morriss <whit.morriss@canonical.com>
2015-05-01 15:14:06 -0500

    Working prototype of the docker_opts ansible module
    How it works (in ansible):
    
     - name: add a label
       docker_opts: yaml=./myopts.yml action=add key=labels val=staging
    
     - name: add another label
       docker_opts: yaml=./myopts.yml action=add key=labels val=ssd
    
     - name: add a non repeating flag
       docker_opts: "yaml=./myopts.yml action=set key=mtu val={{ flannel_mtu }}"
    
     - name: read vals into global state
       docker_opts: yaml=./myopts.yml
    
     - debug: msg="docker -d {{ docker_daemon_opts }}"
    
    Every mutation resaves the yaml file.  "add" creates and populates sets,
    "remove" depopulates them. "read" generates the actual option string and
    puts into ansibles global state as a convenience.
    
    Requires special branch of charmhelpers to run

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -2,10 +2,11 @@
 from charmhelpers.contrib.ansible import AnsibleHooks
 from charmhelpers.contrib.ansible import install_ansible_support
 import sys
+import os
 
 hook_names = [
-    'config-changed',
     'install',
+    'config-changed',
     'start',
     'stop',
     'upgrade-charm',
@@ -16,13 +17,14 @@ hooks = AnsibleHooks(playbook_path='playbooks/site.yaml',
                      default_hooks=hook_names)
 
 
-@hooks.hook('install', 'upgrade-charm')
-def install():
-    """
-    Install or upgrade ansible.
-    """
-    install_ansible_support(from_ppa=True)
+# @hooks.hook('install', 'upgrade-charm')
+# def install():
+#     """
+#     Install or upgrade ansible.
+#     """
+#     install_ansible_support(from_ppa=True)
+
 
 
 if __name__ == "__main__":
-    hooks.execute(sys.argv)
+    hooks.execute(sys.argv, modules="%s/modules" % os.environ['CHARM_DIR'])
